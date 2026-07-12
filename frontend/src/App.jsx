751e4b4084b5483e74ef23b1276150e5aed27a2a
kunnal-singhh <singhkunal16492@gmail.com>
2026-07-12 11:40:06 +0530

    tailwindcss config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import AssetInventory from './components/AssetInventory';
